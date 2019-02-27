4e5a5975e1566586095aa04750a71abd78ced17e
Lj C <ljcalixte@gmail.com>
2019-02-27 21:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments) {
   var beatles = [];
   for (let i=0; i< musicians.length; i++) {
-    beatles.push('${musicians[i]} plays ${instruments[i]}');
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`);
     }
     return beatles
 }
-
 
 function johnLennonFacts() {
   johnLennonFacts = [
