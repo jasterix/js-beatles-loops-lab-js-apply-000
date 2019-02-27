64563ea676725b674bf836c4df2d62993c26f60d
Lj C <ljcalixte@gmail.com>
2019-02-27 20:58:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  musicians = []
+  instruments = []
+  for (i=0; i< musicians.length; i++) {
+  return '${musicians[i]} plays ${instruments[i]}''
+  }
+}
