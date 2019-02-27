9acda1706a27855eaf16ff966ceceea87580919a
Lj C <ljcalixte@gmail.com>
2019-02-27 21:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments) {
   beatlesLoop = []
   for (let i=0; i< musicians.length; i++) {
-    beatlesLoop.append(musicians[i], ' plays ', instruments[i])
+    beatlesLoop.push(musicians[i], ' plays ', instruments[i])
     }
 }
 
 
 function johnLennonFacts() {
-  
+  johnLennonFacts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+        ]
 }
