673991d392438fc298b979ed0ac5b019bec1f4f4
Lj C <ljcalixte@gmail.com>
2019-02-27 21:06:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
  
 function theBeatlesPlay(musicians, instruments) {
   for (let i=0; i< musicians.length; i++) {
-    return {musicians[i]}, ' plays ', ${instruments[i]}
+    return musicians[i], ' plays ', instruments[i]
     }
