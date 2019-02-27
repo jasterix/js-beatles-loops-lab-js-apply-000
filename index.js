6452c6fbde5af5b431ea71c7723fe3eaf590a631
Lj C <ljcalixte@gmail.com>
2019-02-27 20:58:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   musicians = []
   instruments = []
+  
   for (i=0; i< musicians.length; i++) {
   return '${musicians[i]} plays ${instruments[i]}''
   }
-}
