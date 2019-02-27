46e9fd42c29166aad79d26733d96271dd7e154b1
Lj C <ljcalixte@gmail.com>
2019-02-27 21:20:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
  
 function theBeatlesPlay(musicians, instruments) {
-  beatles = [];
+  var beatles = [];
   for (let i=0; i< musicians.length; i++) {
     beatles.push(`${musicians[i]} plays ${instruments[i]}`);
     }
